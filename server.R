250f54324b0195713cfd31fb9169ecf563f2f56e
James Dalgleish <jamesdalg@gmail.com>
2018-12-02 22:08:34 -0500

    an initial app

---
+++ server.R
@@ -0,0 +1,58 @@
+
+library(leaflet)
+library(magrittr)
+library(rgdal)
+library(rgeos)
+library(maptools)
+library(plotly)
+library(tidyverse)
+library(tigris)
+library(shiny)
+library(sf)
+input<-list(state="nj",chemical="BENZENE")
+county_cancer_chem = readRDS( "cancer_county_chem_pop.rds") %>% 
+  mutate(prevalence = n/pop_est)
+
+options(tigris_class = "sf")
+state_sf <- tigris::counties(state = isolate(input$state)) %>% #it would  load faster if we had all of the objects in RAM, or at least in RDS files.
+#sf::st_simplify(TRUE, dTolerance = 10000) %>% #this reduces the size of the SF object drastically.
+    janitor::clean_names() %>% 
+  mutate(name = tolower(name))
+state_chem <- county_cancer_chem %>% 
+  filter(chemical == isolate(input$chemical) &
+           st == isolate(input$state)) %>% 
+  select(total_rel_summ,county,year) %>% 
+  ungroup() %>% 
+  distinct() %>% 
+  right_join(state_sf, by = c("county" = "name")) %>% 
+  mutate(log_total_rel = log(total_rel_summ + 0.001))
+state_joined_chem <- state_sf %>%
+  left_join(state_chem, by = c("name" = "county")) %>%
+  replace_na(list(total_rel_summ = 0)) %>% 
+  mutate(geometry = geometry.x)
+####state_chem_plot <- ggplot() +
+####  geom_sf(data=state_chem, aes(fill=log((total_rel_summ + 0.001)/aland),group=namelsad, label=namelsad)) +
+  #geom_text(aes(label = name, x = intptlon.y, y = intptlat.y), size = 2) + 
+  # gghighlight::gghighlight(AREA > 0.20) +
+  #ggsflabel::geom_sf_label(data = state_chem, aes(label = namelsad)) +
+####  scale_fill_viridis_c(option = "magma") +
+####  labs(fill = toupper(stringr::str_c(isolate(input$state))," ",tolower(isolate(input$chemical))," releases by county, divided by land area, log transformed")) +
+####  theme(legend.position = "bottom") 
+#plotly::ggplotly(state_chem_plot) #please note that turning this into a ggplotly object will work, but 
+# Define server logic required to draw a histogram
+#I think I'll try leaflet here. It's just not fast enough with ggplot.
+shinyServer(function(session, input, output) {
+   
+  output$leafletplot <- renderLeaflet({
+    
+#    USA <- raster::getData("GADM", country = "usa", level = 2)
+    viridis_pal <- colorNumeric(palette = "viridis", domain = state_chem$log_total_rel, na.color = "grey")
+    leaflet() %>% 
+      addProviderTiles("OpenStreetMap.Mapnik") %>% 
+      addPolygons(data = state_joined_chem, stroke = FALSE, smoothFactor = 0.2, fillOpacity = 0.3,
+                  fillColor = ~viridis_pal(state_chem$log_total_rel))
+    
+    
+  })
+  
+})
---
+++ ui.R
@@ -0,0 +1,25 @@
+library(shiny)
+library(shinycssloaders)
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Old Faithful Geyser Data"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("state","State",tolower(state.abb),"nj"),
+       sliderInput("year",
+                   "Year:",
+                   min = 1987,
+                   max = 2009,
+                   value = 2001)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+       withSpinner(leafletOutput("distPlot"))
+    )
+  )
+))
